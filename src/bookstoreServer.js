1c7faef599547ed31d89452ea4376617c26d4f86
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-18 16:23:01 -0400

    Able to get data from database to terminal

--- src/bookstoreServer.js
+++ src/bookstoreServer.js
@@ -1,34 +1,79 @@
-const express = require('express');
+
+var pg = require('pg');
+const express = require("express");
 const app = express();
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:password@localhost:5432/postgres');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 
-const Bookstores = sequelize.define('bookstores', {
-    name: Sequelize.STRING,
-    address: Sequelize.STRING,
-    imageurl: Sequelize.STRING
-});
+let options = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'password',
+    port: 5432
+}
 
+var pool = new pg.Pool(options);
+let getAllStores = "";
+pool.connect((err, client, done) => {
+    getAllStores = () => {
+        client.query("select * from bookstores")
+            .then(response => {
+                for (var i = 0; i < response.rowCount; i++) {
+                    allEntries = "name: " + response.rows[i].name + " " + "address: " + response.rows[i].address /*+ " " + res.rows[i].imageurl */;
+                    console.log(allEntries);
+                }
+                return allEntries
+            })
+            .then(allEntries => {
+                render(allEntries);
+            })
 
-Bookstores.sync()
-.then(() => {
-    console.log("Bookstores is now ready to be used");
+            .catch(err => { });
+    }
+    // const getAllStores = "select * from bookstores";
+    // client.query(getAllStores)
+    //     .then(response => {
+    //         for (var i = 0; i < response.rowCount; i++) {
+    //             allEntries = "name: " + response.rows[i].name + " " + "address: " + response.rows[i].address /*+ " " + res.rows[i].imageurl */;
+    //             console.log(allEntries);
+    //         }
+    //     })
+    //     .catch(err => { });
+
+    // const addStore = {
+    //     const {name, address, imageurl} = req.body;
+    //     text: "insert into bookstores (name,address,imageurl) values ($1,$2,$3)",
+    //     values: [name, address, imageurl]
+    // }
+    // client.query(addStore)
+    //     .then(response => {
+    //         console.log(response);
+    //     })
 })
-.catch(err => {
-    console.log(err);
-});
+
+function hello() {
+    console.log("you and the people are all voting for the mayor");
+}
+
+app.use(cors());
+
 
 app.get('/bookstores', (req, res) => {
-    Bookstores.findAll({
-        attributes: ['id','name','address','imageurl'],
-    });
-    console.log("Here");
+    //getAllStores();
+
+    hello();
+    console.log("me me me me ")
+    res.send(getAllStores());
+
 });
 
 app.post('/bookstores', (req, res) => {
-    Campus.create({
-        name: req.body.name,
-        address: req.body.address,
-        imageurl: req.body.imageurl
-    });
+
+});
+
+pool.end();
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
 });
