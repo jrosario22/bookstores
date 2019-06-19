e88e49fe8c41f9a796c444c43894552da1ac2427
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-19 17:25:35 -0400

    Removed Test button, using childNodes to get inputs

--- src/testPage.js
+++ src/testPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
-import BookstoreRender from './bookstoreRender';
-
+import BookstoreRender from './bookstoreTemplate';
+import AddStoreForm from './addStoreForm';
 
 class Test extends React.Component {
     constructor(props) {
@@ -16,104 +16,60 @@ class Test extends React.Component {
         })
     }
 
-    componentWillMount() {
-        console.log('Component will mount!')
-    }
-
     componentDidMount() {
         this.dataB();
     }
 
-    // Mounts the form onto the screen after add campus is pressed
     // Will send input data to database
     dataB = () => {
         Axios.get('http://localhost:3000/bookstores')
             .then((response) => {
                 this.setState({
                     bookstores: response.data
-                })
+                });
                 //console.log(response);
                 //console.log(bookstores);
+                console.log('got this:');
                 console.log(response.data);
             })
             .catch(err => console.log(err));
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("Should comp update was called")
-        return this.bookstores !== nextState.bookstores
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-        console.log("will update was called")
-        //this.dataB();
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log('Component did update!')
-        //this.dataB();
-    }
-    componentWillUnmount(){
-        console.log("I unmounted");
-    }
-
     // Will take info from form and send it to the database
-    formPost = (event) => {
-        event.preventDefault();         // Stops page refresh after submit
-        console.log(event.target);
+    formPost = () => {
+        let form = document.getElementById('theForm');
+        console.log(form);
         let data = {
-            name: event.target[0].value,
-            address: event.target[1].value,
-            imageurl: event.target[2].value,
+            name: form.childNodes[0].value,
+            address: form.childNodes[1].value,
+            imageurl: form.childNodes[2].value,
         };
         console.log(data);
         Axios.post('http://localhost:3000/bookstores', data)
             .then((response) => {
-                //console.log(data);
-                this.setState({
-                    data: response
-                });
+                console.log(response);
+                this.dataB();
+
             })
             .catch(function (error) {
                 //Error Text
+                console.log(error);
             })
     }
 
-    // Form
-    renderForm = () => {
-        if (this.state.displayForm === true) {
-            return (
-                //action="http://localhost:3000/campus"
-                <form method="post" onSubmit={this.formPost}>
-                    <input type="text" name="name" placeholder="name" value="Test" />
-                    <input type="text" name="address" placeholder="address" value="test" />
-                    <input type="text" name="imageurl" placeholder="imageurl" value="https://www.welikela.com/wp-content/uploads/2016/01/last-bookstore-book-display.jpg" />
-                    <input type="submit" onClick={this.dataB} />
-                </form>
-            )
-        } else {
-            return null
-        }
-    }
-
-    results = () => {
-        Axios.get("http://localhost:3000/bookstores")
-            .then(res => {
-                //console.log(res)
-            })
-            .catch(err => { })
-    }
     render() {
         const info = this.state.bookstores.map(store => <BookstoreRender name={store.name} address={store.address} imageurl={store.imageurl} />)
         console.log(info);
+        const form = this.handleDisplayValue ? <AddStoreForm dataB={this.dataB} formPost={this.formPost} /> : null;
         return (
             <div>
                 <h1>Test</h1>
                 {/* {this.dataB} */}
                 {info}
-                <button onClick={this.handleDisplayValue}>Test</button>
+                {/* <button onClick={this.handleDisplayValue}>Test</button> */}
                 <br></br>
-                {this.renderForm()}
+                {form}
+                {/* {addStoreForm()} */}
                 <br></br>
                 <br></br>
                 <br></br>
