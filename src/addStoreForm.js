9c86b29a023de5c5b8c8b7913b4f732824d391b0
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-19 17:29:46 -0400

    Changed name of file, removed form tag for div, changed submit to button in order to trigger formPost, closes#3

---
+++ src/addStoreForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Form
+function AddStoreForm(props){
+    // onSubmit={props.formPost}
+    return (
+        //action="http://localhost:3000/campus"
+        
+        <div id="theForm">
+            <input type="text" name="name" placeholder="name" value="Test" />
+            <input type="text" name="address" placeholder="address" value="test" />
+            <input type="text" name="imageurl" placeholder="imageurl" value="https://www.welikela.com/wp-content/uploads/2016/01/last-bookstore-book-display.jpg" />
+            <button onClick={props.formPost}>Click</button> 
+            {/* <input type="submit" onClick={this.dataB()} /> */}
+        </div>
+    )
+}
+
+export default AddStoreForm;
