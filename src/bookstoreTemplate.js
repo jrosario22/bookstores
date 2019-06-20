7e86547b21739936b2b3235080690d4b5d15df58
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-20 11:37:14 -0400

    Changed id to store form

--- src/bookstoreTemplate.js
+++ src/bookstoreTemplate.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function BookstoreRender(props) {
+function BookstoreTemplate(props) {
     return (
         <div>
             <label>{props.name}</label>
             <label>{props.address}</label>
             <img src={props.imageurl} alt="default"/>
-            {/* //edit button
-            // delete button */}
+            <button onClick={props.edit}>Edit</button>
+            <button onClick={props.deleteStore}>Delete</button>
         </div>
     )
 }
 
-export default BookstoreRender;
+export default BookstoreTemplate;
