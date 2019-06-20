f5a3fa68babd2a473a5c672210856c43314f6cfa
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-20 11:39:23 -0400

    Added 2nd test page for books

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { Switch, Route, Link, BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Test from './testPage'
+import Test from './testPage';
+import Test2 from './testPage2';
 
 const homePage = (
     <Router>
@@ -11,10 +12,12 @@ const homePage = (
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/testPage">Test</Link></li>
+                <li><Link to="/testPage2">Test2</Link></li>
             </ul>
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/testPage" component={Test} />
+                <Route path="/testPage2" component={Test2} />
             </Switch>
         </div>
     </Router>
