26a7450387d683dd3c36f9a12407f1c35f062e32
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-20 22:51:57 -0400

    Fixed delete function

--- src/testPage.js
+++ src/testPage.js
@@ -6,7 +6,7 @@ import BookstoreTemplate from './bookstoreTemplate';
 class Test extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { bookstores: [], displayForm: false, id: ''};
+        this.state = { bookstores: [], displayForm: false, id: [] };
     }
 
     // Will hide form until add campus is pressed
@@ -35,12 +35,16 @@ class Test extends React.Component {
 
     // Will take info from form and send it to the database
     addStore = () => {
-        let form = document.getElementById('theForm');
+        let form = document.getElementById('storeForm');
         console.log(form);
+        // let id = {id: };
         let data = {
+            //id: form.childNodes[0].value,
             name: form.childNodes[0].value,
             address: form.childNodes[1].value,
-            imageurl: form.childNodes[2].value
+            imageurl: form.childNodes[2].value,
+            id: form.childNodes[3].value
+
         };
         console.log(data);
         Axios.post('http://localhost:3000/bookstores', data)
@@ -49,36 +53,38 @@ class Test extends React.Component {
                 this.displayStoreData();
             })
             .catch(err => console.log(err))
+    };
+
+
+        deleteStore = (id) => {
+            console.log("delete pressed");
+            Axios.delete('http://localhost:3000/bookstores/'+id)
+                .then(response => {
+                    console.log("info was deleted")
+                    // this.setState({
+                    //     bookstores: response
+                    // })
+                    this.displayStoreData();
+                })
+                .catch(err => console.log(err))
+        };
+
+        render() {
+            const storeInfo = this.state.bookstores.map(store => <BookstoreTemplate name={store.name} address={store.address} imageurl={store.imageurl} id={store.id} deleteStore={this.deleteStore} />)
+            // console.log(storeInfo);
+            const rendStore = this.handleDisplayValue ? <AddStoreForm displayStoreData={this.displayStoreData} addStore={this.addStore} /> : null;
+            return (
+                <div>
+                    <h1>Test</h1>
+                    {/* {this.dataB} */}
+                    {storeInfo}
+                    <br></br>
+                    {rendStore}
+                    <br></br>
+                    <br></br>
+                </div>
+            )
+        }
     }
 
-    deleteStore = () => {
-        console.log("delete pressed");
-        Axios.delete('http://localhost:3000/bookstores/{this.state.id}')
-            .then(response => {
-                console.log("info was deleted")
-                // this.setState({
-                //     bookstores: response
-                // })
-            })
-            .catch(err => console.log(err))
-    }
-
-    render() {
-        const storeInfo = this.state.bookstores.map(store => <BookstoreTemplate name={store.name} address={store.address} imageurl={store.imageurl} />)
-        console.log(storeInfo);
-        const storeForm = this.handleDisplayValue ? <AddStoreForm displayStoreData={this.displayStoreData} formPost={this.addStore} /> : null;
-        return (
-            <div>
-                <h1>Test</h1>
-                {/* {this.dataB} */}
-                {storeInfo}
-                <br></br>
-                {storeForm}
-                <br></br>
-                <br></br>
-            </div>
-        )
-    }
-}
-
-export default Test;
+    export default Test;
